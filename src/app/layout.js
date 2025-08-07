f1bdf67e8dd3b3da196e1c2e575006d0b317face
parsapursmaili <parsapursmaili@gmail.com>
2025-08-07 19:57:49 +0330

    و باز هم آنالیتیکس

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@
 import "./globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import { vazir } from "./font";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
